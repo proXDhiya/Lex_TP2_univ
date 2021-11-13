b8e15094e8518c9a39461943f0a9593be359ee65
proXDhiya <prox.dhiya@gmail.com>
2021-11-13 17:31:03 +0100

    script to compile lex code

---
+++ compile.sh
@@ -0,0 +1,9 @@
+# @proXDhiya
+# auto compiler from lex to binary file
+# $1 first arg represent lex file PATH
+# $2 secend arg represent output file PATH
+
+lex $1
+gcc lex.yy.c -o $2
+rm lex.yy.c
+./$2
